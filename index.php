623e12311b6aaa932b348b077e7b551204b29d08
EUNJI-JEON <jej.career@gmail.com>
2019-03-25 20:40:42 +0900

    index and head of search page is done

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Doodle</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+  </head>
+  <body>
+
+    <div class="wrapper indexPage">
+      <div class="mainSection">
+          <div class="logoContainer">
+            <img src="assets/images/doodleLogo.png" alt="">
+          </div>
+
+          <div class="searchContainer">
+            <form class="" action="search.php" method="GET">
+              <input class="searchBox" type="text" name="term">
+              <input class="searchButton" type="submit" value="Search">
+
+            </form>
+
+
+          </div>
+
+      </div>
+
+    </div>
+
+  </body>
+</html>
---
+++ search.php
@@ -0,0 +1,81 @@
+<?php
+    if(isset($_GET["term"])){
+      $term = $_GET["term"];
+    }
+    else{
+      exit("You must enter a search term");
+    }
+
+    $type= isset($_GET["type"]) ? $_GET["type"] : "sites";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Doodle</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+  </head>
+  <body>
+    <div class="wrapper">
+
+        <div class="header">
+
+          <div class="headerContent">
+
+            <div class="logoContainer">
+              <a href="index.php">
+                <img src="assets/images/doodleLogo.png" alt="">
+              </a>
+            </div>
+
+            <div class="searchContainer">
+              <form action="search.php" method="GET">
+                <div class="searchBarContainer">
+                  <input class="searchBox" type="text" name="term">
+                  <button class="searchButton">
+                    <img src="assets/images/icons/search.png" alt="">
+                  </button>
+                </div>
+
+              </form>
+
+            </div>
+
+
+          </div>
+
+
+
+          <div class="tabsContainer">
+            <ul class="tablist">
+              <li class="<?php echo $type == 'sites'? 'active' : '' ?>">
+                <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                  Sites
+                </a>
+              </li>
+
+              <li class="<?php echo $type == 'images'? 'active' : '' ?>">
+                <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                  Images
+                </a>
+              </li>
+
+            </ul>
+
+          </div>
+
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+  </body>
+</html>
