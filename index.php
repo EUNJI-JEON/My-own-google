332be4a8fde7170b44a62e1e661264d148668da2
EUNJI-JEON <jej.career@gmail.com>
2019-03-27 02:23:58 +0900

    web crawling is done

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome to Doodle</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Search the web for sites and images.">
+    <meta name="keywords" content="Search engine, doodle, websites">
+    <meta name="author" content="Eunji Jeon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="assets/css/style.css">
 
@@ -12,7 +17,7 @@
     <div class="wrapper indexPage">
       <div class="mainSection">
           <div class="logoContainer">
-            <img src="assets/images/doodleLogo.png" alt="">
+            <img src="assets/images/doodleLogo.png" title="Logo of our site" alt="Site logo">
           </div>
 
           <div class="searchContainer">
